aa900ce579ede1b702627a18a0817753049a5ae9
Nicolas Sayer <nicolascsayer@gmail.com>
2024-02-08 19:29:21 +0100

    cache

--- project-complexity/index.js
+++ project-complexity/index.js
@@ -14,84 +14,93 @@ const addComplexityDiv = () => {
         const rowsElement = childElement.querySelectorAll('div[role="row"]');
 
         if (titleElement && rowsElement) {
-          const complexityValues = Array.from(rowsElement)
-            .map((row) => {
-              const complexityCell = row.querySelector(
-                'div[data-testid*="Complexity"]'
-              );
+          const isOpened =
+            titleElement.firstElementChild &&
+            titleElement.firstElementChild.firstElementChild &&
+            titleElement.firstElementChild.firstElementChild
+              .firstElementChild &&
+            titleElement.firstElementChild.firstElementChild.firstElementChild
+              .getAttribute("class")
+              .endsWith("octicon-chevron-down");
 
-              if (
-                complexityCell &&
-                complexityCell.firstElementChild &&
-                complexityCell.firstElementChild.firstElementChild &&
-                complexityCell.firstElementChild.firstElementChild
-                  .firstElementChild &&
-                complexityCell.firstElementChild.firstElementChild
-                  .firstElementChild.firstElementChild &&
-                complexityCell.firstElementChild.firstElementChild
-                  .firstElementChild.firstElementChild.firstElementChild
-              ) {
-                return complexityCell.firstElementChild.firstElementChild
-                  .firstElementChild.firstElementChild.firstElementChild
-                  .textContent;
+          if (isOpened) {
+            const complexityValues = Array.from(rowsElement)
+              .map((row) => {
+                const complexityCell = row.querySelector(
+                  'div[data-testid*="Complexity"]'
+                );
+
+                if (
+                  complexityCell &&
+                  complexityCell.firstElementChild &&
+                  complexityCell.firstElementChild.firstElementChild &&
+                  complexityCell.firstElementChild.firstElementChild
+                    .firstElementChild &&
+                  complexityCell.firstElementChild.firstElementChild
+                    .firstElementChild.firstElementChild &&
+                  complexityCell.firstElementChild.firstElementChild
+                    .firstElementChild.firstElementChild.firstElementChild
+                ) {
+                  return complexityCell.firstElementChild.firstElementChild
+                    .firstElementChild.firstElementChild.firstElementChild
+                    .textContent;
+                }
+
+                return null;
+              })
+              .filter(Boolean);
+
+            const titleId = titleElement.getAttribute("data-testid");
+
+            complexitySums[titleId] = complexityValues.reduce((acc, cur) => {
+              switch (cur) {
+                case "15 mins":
+                  return acc + 0.1;
+                case "1 hour":
+                  return acc + 0.2;
+                case "\u{BD} day":
+                  return acc + 0.5;
+                case "1 day":
+                  return acc + 1;
+                case "2-3 days":
+                  return acc + 3;
+                case "4-5 days":
+                  return acc + 5;
+                case "+5 days":
+                  return acc + 6;
+                default:
+                  return acc;
               }
+            }, 0);
+          }
 
-              return null;
-            })
-            .filter(Boolean);
+          const subChildElement = titleElement.firstElementChild;
 
-          const titleId = titleElement.getAttribute("data-testid");
+          if (subChildElement && complexitySums[titleId]) {
+            const divId =
+              groupElement.dataset.testid.replace(/\s/g, "-") + "-counter";
 
-          complexitySums[titleId] = complexityValues.reduce((acc, cur) => {
-            switch (cur) {
-              case "15 mins":
-                return acc + 0.1;
-              case "1 hour":
-                return acc + 0.2;
-              case "\u{BD} day":
-                return acc + 0.5;
-              case "1 day":
-                return acc + 1;
-              case "2-3 days":
-                return acc + 3;
-              case "4-5 days":
-                return acc + 5;
-              case "+5 days":
-                return acc + 6;
-              default:
-                return acc;
-            }
-          }, 0);
+            const number = Math.round(complexitySums[titleId] * 2) / 2;
 
-          if (complexityValues.length || complexitySums[titleId]) {
-            const subChildElement = titleElement.firstElementChild;
+            const divContent = `${number} day${number > 1 ? "s" : ""}`;
 
-            if (subChildElement) {
-              const divId =
-                groupElement.dataset.testid.replace(/\s/g, "-") + "-counter";
+            let div = document.getElementById(divId);
 
-              const number = Math.round(complexitySums[titleId] * 2) / 2;
+            if (div) {
+              div.innerHTML = divContent;
+            } else {
+              div = document.createElement("div");
+              div.id = divId;
+              div.innerHTML = divContent;
 
-              const divContent = `${number} day${number > 1 ? "s" : ""}`;
-
-              let div = document.getElementById(divId);
-
-              if (div) {
-                div.innerHTML = divContent;
-              } else {
-                div = document.createElement("div");
-                div.id = divId;
-                div.innerHTML = divContent;
-
-                div.style.borderRadius = "999px";
-                div.style.backgroundColor = "rgb(221, 244, 255)";
-                div.style.borderColor = "rgba(84, 174, 255, 0.4)";
-                div.style.borderStyle = "solid";
-                div.style.borderWidth = "1px";
-                div.style.paddingLeft = "8px";
-                div.style.paddingRight = "8px";
-                subChildElement.appendChild(div);
-              }
+              div.style.borderRadius = "999px";
+              div.style.backgroundColor = "rgb(221, 244, 255)";
+              div.style.borderColor = "rgba(84, 174, 255, 0.4)";
+              div.style.borderStyle = "solid";
+              div.style.borderWidth = "1px";
+              div.style.paddingLeft = "8px";
+              div.style.paddingRight = "8px";
+              subChildElement.appendChild(div);
             }
           }
         }
